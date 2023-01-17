9a7c8db5c0b343ec2a5526005f8ee4ddacf66fe3
khang <khang.td194591@sis.hust.edu.vn>
2023-01-17 11:51:04 +0700

    Add image on book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -58,13 +58,14 @@ class BooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def book_params
-      params.require(:book).permit(:title, :number_of_pages)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def book_params
+    params.require(:book).permit(:title, :number_of_pages, :image)
+  end
 end
